b1bc9901962e96614adfee02bba58955d33307f4
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-05-14 15:06:34 -0300

    Implementa Banner

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { GlobalStyle } from './assets/css/global';
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <GlobalStyle />
+      <GlobalStyle />
       <App />
       <ToastContainer />
     </BrowserRouter>
--- front-end/src/views/Home/index.tsx
+++ front-end/src/views/Home/index.tsx
@@ -1,19 +1,41 @@
 import { Button, Container } from "react-bootstrap";
-import { toast } from "react-toastify";
 import styled from "styled-components";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import bgMobile from "../../assets/img/bg-fox-entregas-mobile.jpg";
+import bgDesktop from "../../assets/img/bg-fox-entregas.jpg";
 
 export function HomeView () {
     return (
-    <Container>
-        <Title>Fox Entregas</Title>
-      <Button onClick={() => toast.success('Oi')}>Teste</Button>
-      <FontAwesomeIcon icon={faFacebookSquare}/>
-    </Container>
+      <Banner className="vh-100">
+          <Container className="h-100 d-flex flex-column justify-content-center align-items-center align-items-lg-start">
+              <Title className="text-white text-center mt-auto mt-lg-0">Fazemos entregas rápidas, seguras e econômicas</Title>
+              <Button variant='success' size='lg' className="mt-auto mt-lg-3 mb-2">Criar conta</Button>
+              <Button variant='success' size='lg' className="mb-4">Fazer Login</Button>
+          </Container>
+      </Banner>
     )
 }
 
-const Title = styled.p`
-    font-size: 3rem;
+const Banner = styled.div`
+    background: url(${bgMobile}) no-repeat center center;
+    background-size: cover;
+    @media (min-whidth: 576px) {
+        background-image: url(${bgDesktop});
+    }
+    @media (min-width: 768px) {
+        background-image: url(${bgMobile});
+    }
+    @media (min-width: 992px) {
+        background-image: url(${bgDesktop});
+    }
+`
+
+
+const Title = styled.h1`
+    font-size: 2.25rem;
+    font-weight: bold;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); 
+    @media (min-width: 992px) {
+        font-size: 3rem;
+        max-width: 500px;
+    }
 `
