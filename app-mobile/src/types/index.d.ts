44fa1581c677b68912f4185461da21e3b950c7b9
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-06-24 11:55:35 -0300

    exibe logo na home.

--- app-mobile/src/types/index.d.ts
+++ app-mobile/src/types/index.d.ts
@@ -1,2 +1,9 @@
 /* eslint-disable prettier/prettier */
 declare module '*.jpg';
+
+declare module '*.svg' {
+  import React from 'react';
+  import {SvgProps} from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
--- app-mobile/src/views/Home/index.tsx
+++ app-mobile/src/views/Home/index.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {ImageBackground, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import bg from '../../assets/img/bg-fast-entregas-mobile.jpg';
+import Logo from '../../assets/img/logo-fast-entregas-white.svg';
 
 export function HomeView() {
   return (
     <ImageBackground source={bg} style={styles.background}>
       <SafeAreaView style={styles.view}>
-        <Text>Logo</Text>
+        <Logo width={266} height={70}/>
         <Text>Faça login e escolha os pedidos que quer entregar.</Text>
         <Text>Fazer login</Text>
       </SafeAreaView>
