9294159f8d6dcb564f2fa52ce24f1513a070c6cb
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-06-27 13:39:19 -0300

    personaliza botão de logout.

--- app-mobile/src/components/LogoutButton/index.tsx
+++ app-mobile/src/components/LogoutButton/index.tsx
@@ -1,11 +1,38 @@
 /* eslint-disable prettier/prettier */
+import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons/faRightFromBracket';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import {Button} from 'react-native';
-import { logoutUser } from '../../services/logoutUser';
+import {StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import {logoutUser} from '../../services/logoutUser';
 
 export function LogoutButton() {
-    const handleLogout = async () => {
-       await logoutUser();
-    };
-  return <Button title="Sair" onPress={handleLogout} />;
+  const handleLogout = async () => {
+    Alert.alert(
+      'Tem certeza que deseja sair?',
+      'Você precisará informar o e-mail e senhha no próximo acesso.',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Ok',
+          onPress: async () => {
+            await logoutUser();
+          },
+        },
+      ],
+    );
+  };
+  return (
+    <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
+      <FontAwesomeIcon icon={faRightFromBracket} color="#FFF" size={18} />
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create({
+  logoutButton: {
+    padding: 10,
+  },
+});
